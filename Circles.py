8366cd6f3c5b0c71d6a4a05dbb89f971d274e028
Ben <ben_j_jury@hotmail.co.uk>
2024-08-22 18:27:12 +0100

    Start of main.py

--- Circles.py
+++ Circles.py
@@ -2,17 +2,17 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.collections as mc
 
-def generate_circles(num, radius, max_position):
+def generate_circles(num, max_position):
     xy = max_position * np.random.random((num, 2))
     return xy
     
 def draw_circles(xy, radius):
     
     plt.rcParams["figure.figsize"] = [7.50, 5]
-    plt.rcParams["figure.autolayout"] = True
+#    plt.rcParams["figure.autolayout"] = True
     
     num = len(xy[0])
-    sizes = [40] * num
+    sizes = [radius] * num
     fig, ax = plt.subplots()
     collection = mc.CircleCollection(sizes, offsets=xy, transOffset=ax.transData, color='blue')
     ax.add_collection(collection)
@@ -21,7 +21,7 @@ def draw_circles(xy, radius):
     
 def gravity(num, radius, max_position, g, dt, time_steps):
     velocity = [[0 for x in range(num)] for y in range(time_steps)]
-    xy = generate_circles(num, radius, max_position)
+    xy = generate_circles(num, max_position)
     y = [[0 for x in range(num)] for y in range(time_steps)]
     for j in range(num):
         y[0][j] = xy[j][1]
@@ -34,13 +34,13 @@ def gravity(num, radius, max_position, g, dt, time_steps):
     
 def main():
     num = 10
-    radius = 10
+    radius = 50
     max_position = 10
     g = 9.8
     dt = 0.1
     time_steps = 10
     
-    xy = generate_circles(num, radius, max_position)
+    xy = generate_circles(num, max_position)
     velocity, y = gravity(num, radius, max_position, g, dt, time_steps)
     draw_circles(xy, radius)    
     print("Initial Co-ordinates:", xy)
