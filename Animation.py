c7c1707774257413c075130f0563ec6ae7ae04a6
BenJury22 <ben_j_jury@hotmail.co.uk>
2024-08-27 20:01:58 +0100

    animation progress

--- Animation.py
+++ Animation.py
@@ -13,22 +13,34 @@ def draw_frame(xy, xy_boundary):
 
     plt.show()
 
-
-def simulate_steps():
-    return 0
-
+#Arbitrary function at the moment
+def simulate_steps(initial_pos, velocity, dt, time_steps):
+    num = len(initial_pos)
+    x = np.zeros((time_steps, num))
+    y = np.zeros((time_steps, num))
+    x[0, :] = initial_pos[:, 0]
+    y[0, :] = initial_pos[:, 1]
+    for i in range(time_steps - 1):
+        x[i + 1] = x[i] + velocity * dt
+        y[i + 1] = y[i]
+    return x, y
 
 def test_animate():
     num = 3
     xy_boundaries = [10, 10]
     xy = IC.initialise_particles(num, xy_boundaries)
     print(xy)
-    print()
-    print(xy[:, 0])                       #This is equal to x
-    print(xy[:, 1])                       #This is equal to y
-    print()
-    
+
+    velocity = 1
+    dt = 0.01
+    time_steps = 10
+    x, y = simulate_steps(xy, velocity, dt, time_steps)
+    print(x)
+    print(y)
+
     draw_frame(xy, xy_boundaries)
+    
+
 
 
 
