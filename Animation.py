4de8a3e74a5121622e46569b47fa6804a1bc4272
BenJury22 <ben_j_jury@hotmail.co.uk>
2024-08-28 14:41:29 +0100

    Removed function which produced data

--- Animation.py
+++ Animation.py
@@ -4,37 +4,32 @@ import numpy as np
 
 class AnimatedScatter(object):
 
-    def __init__(self, num=500):
+    def __init__(self, num=500, data_stream_func=None, cmap="seismic", 
+                 point_size=40, xlim=(-10, 15), ylim=(-10, 15), interval=5):
         self.num = num
-        self.stream = self.data_stream()
+        self.stream = data_stream_func(self.num) if data_stream_func else self.data_stream()
+
+        self.cmap = cmap
+        self.point_size = point_size
+        self.xlim = xlim
+        self.ylim = ylim
+        self.interval = interval
 
         self.fig, self.ax = plt.subplots()                                      # Setup the figure and axes
-        self.ani = animation.FuncAnimation(self.fig, self.update, interval=5,    # Setup FuncAnimation (this calls setup_plot and update)
+        self.ani = animation.FuncAnimation(self.fig, self.update, interval=self.interval,    # Setup FuncAnimation (this calls setup_plot and update)
                                           init_func=self.setup_plot, blit=True)
-
 
     def setup_plot(self):
         x, y, c = next(self.stream).T                                              #Collect values from data_stream
         
         self.ax.xaxis.set_ticks([])                                                #Remove axis labels
         self.ax.yaxis.set_ticks([])
-        self.scat = self.ax.scatter(x, y, c=c, s=40, vmin=0, vmax=1,                #Plot scatter graph
-                                        cmap="seismic", edgecolor="k")
-        self.ax.axis([-10, 15, -10, 15])
-
+        self.scat = self.ax.scatter(x, y, c=c, s=self.point_size, vmin=0, vmax=1,    # Plot scatter graph
+                                    cmap=self.cmap, edgecolor="k")
+        self.ax.axis([self.xlim[0], self.xlim[1], self.ylim[0], self.ylim[1]])
         return self.scat,
-    
-    def data_stream(self):
-        xy = (np.random.random((self.num, 2))-0.5)*10    #Maybe incorperate IC.initialise_particles (would need to change format of output)
-        c = np.random.random(self.num).T                 #Define nitial colour
-        velocity = 1
-        while True:
-            velocity += 0.1
-            xy += np.ones((self.num, 2)) * 0.001 * velocity    #Update xy positions (currently moves along y diagonal line)
-            c += 0.0005 * (xy[:,0] - xy[:,1])                   #Updates colour (we may want a meaningful colour dependency)
-            yield np.c_[xy[:,0], xy[:,1], c]                   #return an array of current [x, y, c] values
 
-        
+
     def update(self, i):
         data = next(self.stream)                            #Collect the next timesteps set of data
         self.scat.set_offsets(data[:, :2])                  #x and y values
@@ -42,9 +37,5 @@ class AnimatedScatter(object):
 
         return self.scat,
 
-
-if __name__ == '__main__':
-    a = AnimatedScatter()
-    plt.show()
 
 
