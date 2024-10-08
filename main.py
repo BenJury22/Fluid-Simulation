15947db4250f99eb352e8c96304fe33cbd6699fc
Simon <simon_e_jury@hotmail.com>
2024-10-08 18:56:17 +0100

    Clean up main

--- main.py
+++ main.py
@@ -11,32 +11,36 @@ import Animation as an
 
 
 def main():
-    # Hard coded variables, to later be replaced by GUI or parameter file.
-    # Time Variables
+    # Time variables
     time_step = 0.01
 
-    # Force Variables
+    # Force variables
     phys_constants = {"Gravitational_Acceleration": 9.81, "Viscosity_Strength": 0.1, "Pressure_Strength": 5, "Near_Pressure_Strength": 1}
 
     # IC & BC variables
     num = 500
     xy_boundaries = [10, 10]
     xy_max_v = [5, 5]
+
+    # Smoothing parameters
     smoothing_radius = 0.5
     near_smoothing_radius = 0.2
 
-    # Generate Initial Conditions
+    # Generate initial conditions
     initial_position = IC.initialise_particles(num, xy_boundaries)
     initial_velocity = IC.initialise_velocity(num, xy_max_v)
 
+    # Initialise UI
     root = tk.Tk()
     root.geometry("1500x900")
     root.title("Fluid Simulation")
 
+    # Create UI constnats label
     for key, value in phys_constants.items():
         label = tk.Label(root, text=f"{key} = {value}", font = ('arial', 14))
         label.pack()
 
+    # Run simulation and animation
     Animation = an.AnimatedScatter(data_stream_func=new_pos, root = root,
                     cmap="rainbow", point_size=30, 
                     xlim=(0, xy_boundaries[0]), ylim=(0, xy_boundaries[1]), 
@@ -49,9 +53,12 @@ def main():
                     smoothing_radius = smoothing_radius,
                     near_smoothing_radius = near_smoothing_radius)
 
+    # End UI loop
     root.mainloop()
 
+
 def new_pos(time_steps=0, position=0, velocity=0, phys_constants=0, boundary_conditions=0, smoothing_radius = 0, near_smoothing_radius = 0):
+    # Calculates next frame of simulation and yield result
     while True:
         # Calculate changes in velocity due to forces
         gravity_dv = forces.apply_gravity(position, time_steps, phys_constants["Gravitational_Acceleration"])
@@ -66,16 +73,10 @@ def new_pos(time_steps=0, position=0, velocity=0, phys_constants=0, boundary_con
         # Apply boundary conditions
         position, velocity = BC.apply_BC(position, velocity, boundary_conditions)
 
-        # Finding colour (dependent on density)
-
-        average_density = forces.Av_density(len(position), boundary_conditions)
-        c = densities / average_density
-
-        # Finding Colour (dependent on speed)
-#        c = forces.velocity_mag(velocity)
+        # Define colour dependent on speed
+        c = forces.velocity_mag(velocity)
     
-
-        yield np.c_[position[:,0], position[:,1], c/3] 
+        yield np.c_[position[:,0], position[:,1], c/6] 
 
 
 if __name__ == '__main__':
